e67fe860661d0c661a61e8bcd45703e37d6d50e8
Rushsky <lastkiss563@gmail.com>
2026-01-12 20:56:48 +0800

    add include,src;add ddl

---
+++ build/CMakeFiles/common.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/common.dir/src/pnc_map.cpp.obj"
+  "CMakeFiles/common.dir/src/pnc_map.cpp.obj.d"
+  "CMakeFiles/common.dir/src/process.cpp.obj"
+  "CMakeFiles/common.dir/src/process.cpp.obj.d"
+  "D:/vcode/cmake_test1/cmake_planning_test1/bin/libcommon.dll"
+  "D:/vcode/cmake_test1/cmake_planning_test1/bin/libcommon.dll.manifest"
+  "D:/vcode/cmake_test1/cmake_planning_test1/bin/libcommon.pdb"
+  "libcommon.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/common.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
